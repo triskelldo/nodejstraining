abd8f8e1ed8031f0306e45b29320825677ab7013
Vincent Cailleaud <vincent.cailleaud@triskellsoftware.fr>
2023-10-10 14:55:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const _ = require('lodash');
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const concatenatedArray =
+_.concat(array1, array2);
+console.log('Tableau concaténé :',
+concatenatedArray);
